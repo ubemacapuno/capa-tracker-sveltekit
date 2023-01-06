30ae62d139f89d46f141be758a01d2e85c381e5d
Fast DMS Corey <fastdmscorey@Coreys-MacBook-Air.local>
2023-01-06 12:48:05 -0700

    Added hooks file.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,23 @@
 	import "../app.css";
 </script>
 <div class="flex flex-col min-h-screen relative pb-7">
+
+
+	<ul class="menu menu-horizontal px-1 z-50">
+		<li>
+			<button> Set Theme 🎨 </button>
+			<ul class="p-2 bg-base-100 w-full max-h-96 overflow-y-scroll">
+				<li>
+					<button>Dark</button>
+				</li>
+				<li>
+					<button>Light</button>
+				</li>
+			</ul>
+		</li>
+	</ul>
+
+
 	<Header />
 		<div class="hero min-h-screen bg-base-200 relative mb-5">
 			<div class="hero-content">
