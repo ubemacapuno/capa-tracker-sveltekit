b971487aa8a159443a0aa1289dcbe2426dd2e873
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2023-01-21 17:40:23 -0700

    Updated load function

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,7 @@ import type { Actions } from "./$types";
 import { redirect } from "@sveltejs/kit";
 import { capaReports } from "$capas"
 import type { LayoutServerLoad } from './$types';
+import { fix_pojo } from "$utilities/fix_pojo";
 
 export const load: LayoutServerLoad = async (event) => {
 	const data = await capaReports.find({}, {limit: 500, projection: {
@@ -17,7 +18,7 @@ export const load: LayoutServerLoad = async (event) => {
 	}}).toArray();
 
   return {
-	capaReports: JSON.parse(JSON.stringify(data)),
+	capaReports: fix_pojo(data),
     session: await event.locals.getSession()
   };
 }; 
