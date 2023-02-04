035f1204f52d36b22162cc3dbd1904c0bf247aeb
corey <damoclescj@gmail.com>
2023-02-04 16:39:40 -0700

    fix(Updated dashboard): Updated dashboard
    Updated dashboard overdue capas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { signIn} from "@auth/sveltekit/client"
-	import { page } from "$app/stores"
+	import { signIn } from '@auth/sveltekit/client';
+	import { page } from '$app/stores';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-    $: ({capaReports} = data)
+	$: ({ capaReports } = data);
 </script>
 
 <svelte:head>
@@ -12,56 +12,43 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-
 {#if !$page.data.session}
 	<div class="flex flex-col justify-center">
 		<h1 class="text-3xl text-warning font-bold">Please sign in for complete access!</h1>
-		<p class="mt-5 text-error text-md italic">*NOTE: CAPA Tracker is under construction and authentication may become disconnected from production, preventing login access!*</p>
-		<button class="mt-10 btn btn-primary" on:click={() => signIn("github")}>Login</button>
+		<p class="mt-5 text-error text-md italic">
+			*NOTE: CAPA Tracker is under construction and authentication may become disconnected from
+			production, preventing login access!*
+		</p>
+		<button class="mt-10 btn btn-primary" on:click={() => signIn('github')}>Login</button>
 	</div>
-
 {:else}
-
 	<h1 class="text-3xl font-bold text-primary">Dashboard</h1>
-	<p>Signed in as <span class="text-accent">{$page.data.session.user?.name ?? "User"}</span></p>
-	
+	<p>Signed in as <span class="text-accent">{$page.data.session.user?.name ?? 'User'}</span></p>
+
 	<div class="mt-5 bg-base-200 stats stats-vertical w-full">
 		<div class="stat">
 			<div class="stat-title">Total CAPAs Open</div>
 			<div class="stat-value">
-				{capaReports
-					.filter(capaReport => capaReport.capaStatus !== "Closed")
-					.length
-				}
+				{capaReports.filter((capaReport) => capaReport.capaStatus !== 'Closed').length}
 			</div>
 		</div>
 		<div class="stat text-error">
 			<div class="stat-title">CAPAs Overdue</div>
 			<div class="stat-value">
-				{capaReports
-					.filter(capaReport => new Date(capaReport.currentPhaseDueDate) < new Date())
-					.length
-
-					-
-					
-				capaReports.
-					filter(capaReport => capaReport.capaStatus === "Closed").length 
-				}
+				{capaReports.filter((capaReport) => new Date(capaReport.currentPhaseDueDate) < new Date())
+					.length}
 			</div>
 		</div>
 		<div class="stat text-accent">
 			<div class="stat-title">Total CAPAs Closed</div>
 			<div class="stat-value">
-				{capaReports
-					.filter(capaReport => capaReport.capaStatus === "Closed")
-					.length
-				}
+				{capaReports.filter((capaReport) => capaReport.capaStatus === 'Closed').length}
 			</div>
 		</div>
 		<div class="stat">
 			<div class="stat-title">Total CAPAs</div>
 			<div class="stat-value">{capaReports.length}</div>
-		</div>	
+		</div>
 	</div>
 	<div class="max-w-md">
 		<a href="/capas" class="my-6 btn btn-primary">View CAPAs</a>
