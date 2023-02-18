48c31b05a919fa91a0979de3888704659b4c87be
corey <damoclescj@gmail.com>
2023-02-18 16:18:48 -0700

    feat(Auth for capas/[_id] page): Added auth for capas/[_id] page
    Added auth for capas/[_id] page by using +layout.server.ts
    
    BREAKING CHANGE: It is recommended NOT to use +layout.server.ts for auth. This will need to be updated.

---
+++ src/routes/capas/+layout.server.ts
@@ -0,0 +1,11 @@
+import type { LayoutServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async function (event) {
+	const session = await event.locals.getSession();
+	if (!session?.user) throw redirect(303, '/');
+
+	return {
+		session: await event.locals.getSession()
+	};
+} satisfies LayoutServerLoad;
