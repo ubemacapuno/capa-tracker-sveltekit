1e07cfab8e60d4de86335d0cb8a883221bd2bfb0
corey <damoclescj@gmail.com>
2023-03-04 16:30:58 -0700

    build(Updated config): Updated config
    Updated config

--- svelte.config.js
+++ svelte.config.js
@@ -9,15 +9,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			// edge: false,
-			runtime: 'nodejs18.x',
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
+			runtime: 'nodejs18.x'
 		}),
 		alias: {
 			$db: './src/db',
