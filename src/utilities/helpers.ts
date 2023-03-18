ecdee62090d639dafe3f16435a5ab8ffd3681a5a
corey <damoclescj@gmail.com>
2023-03-18 14:04:35 -0700

    Updated avatarLetters helper function

--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -1,7 +1,7 @@
 //Helper function for Letters in Avatar:
 export function avatarLetters(name: string): string {
 	const names = name.split(' ');
-	if (names.length === 0) return 'Me';
+	if (names.length === 0 || name === 'undefined') return 'Me';
 	if (names.length === 1) {
 		const firstLetter = names[0][0];
 		return firstLetter.toUpperCase();
